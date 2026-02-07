7bb890c4efe99e38fc73dcf5afc63d5c90c9c70b
6970339-spec <6970339@gmail.com>
2026-02-07 14:53:38 +0200

    Add fallback flutter config for plugin builds

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -2,10 +2,16 @@ pluginManagement {
     val flutterSdkPath =
         run {
             val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
+            val localProperties = file("local.properties")
+            if (localProperties.exists()) {
+                localProperties.inputStream().use { properties.load(it) }
+            }
+            val propertyPath = properties.getProperty("flutter.sdk")
+            val envPath = System.getenv("FLUTTER_ROOT") ?: System.getenv("FLUTTER_HOME")
+            require(!propertyPath.isNullOrBlank() || !envPath.isNullOrBlank()) {
+                "flutter.sdk not set in local.properties or FLUTTER_ROOT/FLUTTER_HOME"
+            }
+            propertyPath ?: envPath!!
         }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
